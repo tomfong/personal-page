79025e96b0ac520585708e3d5230d5d9c5261310
Tom Fong <lhfongaa@connect.ust.hk>
2021-01-25 01:24:28 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tom Fong</h1>
+        <p>Coming soon...</p>
+        <p>Welcome to check out my <a href="https://github.com/tomfong" target="_blank">GitHub</a>!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
