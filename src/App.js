0ebcfcf970dffa9f897cdfdd8c61fb94cec732e0
Tom Fong <tom.fonglamhang@gmail.com>
2021-04-22 20:41:58 +0800

    prepare split-pane

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import SplitPane, { Pane }  from 'react-split-pane';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Tom Fong</h1>
-        <p>Coming soon...</p>
-        <p>Welcome to check out my <a href="https://github.com/tomfong" target="_blank">GitHub</a>!</p>
-      </header>
+      <SplitPane split="vertical" allowResize={true}>
+        <Pane initialSize="50%" className="Left">
+          <h1>Tom Fong</h1>
+          <p>Coming soon...</p>
+          <p>Welcome to check out my <a href="https://github.com/tomfong" target="_blank">GitHub</a>!</p>
+        </Pane>
+        <Pane initialSize="50%" className="Right">
+          <h1>Tom Fong</h1>
+          <p>Coming soon...</p>
+          <p>Welcome to check out my <a href="https://github.com/tomfong" target="_blank">GitHub</a>!</p>
+        </Pane>
+      </SplitPane>
     </div>
   );
 }
